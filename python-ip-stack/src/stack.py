0b883796d1016591bf78194846ce1277a4dc9da1
Damiao Rodrigues <adamiaonr@gmail.com>
2017-12-30 21:45:38 +0000

    changed from pytun to direct direct use of '/dev/net/tun' and ioctl
    - now icmp works without workarounds
    - arp responses are correct

--- python-ip-stack/src/stack.py
+++ python-ip-stack/src/stack.py
@@ -8,7 +8,7 @@ import thread
 from socket import *
 
 from tap import Tap
-from ethernet import Ethernet
+from ethernet import Ethernet, mac_to_str
 from arp import ARP_Module
 from ipv4 import IPv4_Module, IPv4_Dgram
 from icmp import ICMP_Module
@@ -20,8 +20,8 @@ class Stack:
         # tap device used by stack to send/receive packets
         self.tap = Tap(net_addr)
         # local mac and ip
-        self.mac = self.tap.dev.hwaddr
-        self.ip = int(ipaddress.IPv4Address(unicode(self.tap.dev.addr)))
+        self.mac = self.tap.hw_addr
+        self.ip = int(ipaddress.IPv4Address(unicode(self.tap.net_addr)))
 
         # initialize modules for supported protocols:
         #   - ARP : maintains local ARP table and processes ARP requests
@@ -36,22 +36,18 @@ class Stack:
         #   - ICMP : handles ICMP protocol packets (mainly ping responses)
         self.icmp_mod = ICMP_Module(self)
 
-        # hack : icmp listener to handle icmp packets (Linux doesn't let me 
-        # receive icmp packets with a python tap device)
-        self.icmp_listener_sock = None
-
     def send_frame(self, frame_type, dst_mac, data):
 
         if frame_type in [Ethernet.PROTO_ARP, Ethernet.PROTO_IPv4]:
 
             eth_frame = Ethernet(
-                src_mac      = self.tap.dev.hwaddr,
+                src_mac      = binascii.unhexlify(self.mac.replace(':', '')),
                 dst_mac      = dst_mac,
                 eth_type     = frame_type,
                 payload      = data,
                 payload_size = len(data))
 
-            self.tap.dev.write(eth_frame.pack())
+            self.tap.write(eth_frame.pack())
 
         else:
             print("Stack::send_frame() : [ERROR] unknown frame type : %d" % (frame_type))
@@ -73,30 +69,33 @@ class Stack:
 
         # IPv4 datagram
         elif frame_type == Ethernet.PROTO_IPv4:
+
             eth_frame.unpack_data(raw_frame)
             self.ipv4_mod.process_dgram(eth_frame.get_attr('data', 'payload'))
 
-        else:
-            print("Stack::handle_frame() [ERROR] unknown protocol type : %02x" % (frame_type))
+        # else:
+        #     print("Stack::handle_frame() [ERROR] unknown protocol type : %02x" % (frame_type))
 
-def start_icmp_listener(stack):
+# def start_icmp_listener(stack):
 
-    # hack : i've found it hard to re-direct icmp traffic to the tap 
-    # interface. to circumvent this problem, i try using a raw socket, 
-    # w/ protocol option 'IPPROTO_ICMP'
-    stack.icmp_listener_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
+#     # hack : i've found it hard to re-direct icmp traffic to the tap 
+#     # interface. to circumvent this problem, i try using a raw socket, 
+#     # w/ protocol option 'IPPROTO_ICMP'
+#     stack.icmp_listener_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
 
-    # bind the socket to the tap interface if the python stack
-    stack.icmp_listener_sock.bind((stack.tap.dev.addr, 0))
+#     # bind the socket to the tap interface if the python stack
+#     stack.icmp_listener_sock.bind((stack.tap.dev.addr, 0))
 
-    # recv loop
-    (raw_dgram, snd_addr) = stack.icmp_listener_sock.recvfrom(2048)
+#     # recv loop
+#     (raw_dgram, snd_addr) = stack.icmp_listener_sock.recvfrom(2048)
 
-    # process icmp packet (since proto is IPPROTO_ICMP, we know for sure that 
-    # the received packet will be icmp)
-    icmp_ipv4_dgram = IPv4_Dgram()
-    icmp_ipv4_dgram.unpack(raw_dgram)
-    stack.icmp_mod.process_pckt(icmp_ipv4_dgram)
+#     print("\n******** GOT SOMETHING **********\n")
+
+#     # process icmp packet (since proto is IPPROTO_ICMP, we know for sure that 
+#     # the received packet will be icmp)
+#     icmp_ipv4_dgram = IPv4_Dgram()
+#     icmp_ipv4_dgram.unpack(raw_dgram)
+#     stack.icmp_mod.process_pckt(icmp_ipv4_dgram)
 
 if __name__ == "__main__":
 
@@ -104,16 +103,16 @@ if __name__ == "__main__":
     stack = Stack('10.0.0.4')
     stack.tap.print_info()
 
-    # hack : start icmp listener thread, to overcome the limitations of icmp 
-    # handling in Linux's networking Stack
-    try:
-        thread.start_new_thread(start_icmp_listener, (stack,))
-    except:
-        print("error while starting thread...")
+    # # hack : start icmp listener thread, to overcome the limitations of icmp 
+    # # handling in Linux's networking Stack
+    # try:
+    #     thread.start_new_thread(start_icmp_listener, (stack,))
+    # except:
+    #     print("error while starting thread...")
 
     steps = 100
     while (steps > 0):
-        frame = stack.tap.dev.read(stack.tap.dev.mtu)
+        frame = stack.tap.read()
         stack.handle_frame(frame)
         
         steps -= 1
--- python-ip-stack/src/tap.py
+++ python-ip-stack/src/tap.py
@@ -1,36 +1,133 @@
+import os 
+import sys
+import struct
+import subprocess
+import ipaddress
+import arch
+import socket
+
+# from pytun import TunTapDevice, IFF_TAP, IFF_NO_PI
+# pytun alternative: http://www.secdev.org/projects/tuntap_udp/files/tunproxy.py 
+from fcntl import ioctl
+# custom import
 from ethernet import Ethernet
-from pytun import TunTapDevice, IFF_TAP, IFF_NO_PI
+
+# ioctl constants
+TUNSETIFF = 0x400454ca
+TUNSETPERSIST = 0x400454cb
+
+IFF_TUN = 0x0001
+IFF_TAP = 0x0002
+IFF_NO_PI = 0x1000
+
+SIOCGIFHWADDR = 0x00008927
+
+def run_cmd(cmd, wait = False):
+
+    try:
+
+        p = subprocess.Popen(
+            [cmd], 
+            stdout = subprocess.PIPE,
+            shell = True)
+        p.wait()
+
+        (result, error) = p.communicate()
+        
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write(
+            "%s::run_cmd() : [ERROR]: output = %s, error code = %s\n"
+            % (sys.argv[0], e.output, e.returncode))
+
+    return p.returncode, result, error
 
 class Tap:
 
     def __init__(self, 
-        net_addr = '127.0.0.1', net_mask = '255.255.255.0', 
-        hw_addr = '', mtu = 1500):
+        net_addr = '10.0.0.4', 
+        net_mask = '255.255.255.0', 
+        hw_addr = '', 
+        mtu = 1500):
 
         # create a tap device to send/receive L2 frames
-        tap_dev = TunTapDevice(flags = (IFF_TAP | IFF_NO_PI))
-        tap_dev.persist(True)
-        # set TunTapDevice parameters
-        tap_dev.addr    = net_addr
-        tap_dev.netmask = net_mask
-        # tap_dev.hwaddr  = hw_addr
-        tap_dev.mtu = mtu
-        # set tap device UP
-        tap_dev.up()
+        self.net_addr = net_addr
+        self.net_mask = net_mask
+        self.mtu = mtu
+        self.hw_addr = hw_addr
+        # tap device name
+        # FIXME: now hardcoded, try to fix this
+        self.dev_name = 'tap0'
 
-        # set tap_dev as the stack's device: all network i/o will 
-        # use this device from now on
-        self.dev = tap_dev
+        try:
+            # open tap device
+            tap_fd = os.open('/dev/net/tun', os.O_RDWR)
+            
+            # set tap device flags via ioctl():
+            #
+            # IFF_TUN   : tun device (no Ethernet headers)
+            # IFF_TAP   : tap device
+            # IFF_NO_PI : do not provide packet information, otherwise we end 
+            #             up with unnecessary packet information prepended to 
+            #             the Ethernet frame
+            ifr = struct.pack("16sH", ("%s" % (self.dev_name)), IFF_TAP | IFF_NO_PI)
+            ioctl(tap_fd, TUNSETIFF, ifr)
+
+            # # set device to persistent
+            # ioctl(tap_fd, TUNSETPERSIST, 1)
+
+            print("tap::init() : [INFO] tap device w/ name %s allocated" % (ifr[:16].strip("\x00")))
+            
+            # bring device up and set route for its net address
+            self.up()
+
+        except Exception as e:
+            print("tap::init() : [ERROR] cannot setup tap device (%s)" % (e.message))
+
+        self.fd = tap_fd
+        self.hw_addr = self.get_mac()
+
+    def write(self, data):
+        os.write(self.fd, data)
+
+    def read(self, mtu = 1500):
+        return os.read(self.fd, self.mtu)
+
+    def get_mac(self):
+        ifr = struct.pack('16sH14s', self.dev_name, socket.AF_UNIX, '\x00' * 14)
+        res = ioctl(self.fd, SIOCGIFHWADDR, ifr)
+        address = struct.unpack('16sH14s', res)[2]
+        mac = struct.unpack('6B8x', address)
+        return ":".join(['%02X' % i for i in mac])
+
+    def up(self):
+
+        # bring tap device up
+        if run_cmd('ip link set dev %s up' % (self.dev_name), wait = True)[0]:
+            print("tap::up() : [ERROR] error setting up %s tap device" % (self.dev_name))
+            return 1
+
+        # add route for tap device net address
+        cidr = str(ipaddress.IPv4Network((unicode(".".join(self.net_addr.split(".")[:-1]) + ".0"), unicode(self.net_mask))))
+        if run_cmd('ip route add dev %s %s' % (self.dev_name, cidr), wait = True)[0]:
+            print("tap::up() : [ERROR] error route for %s tap device (%s)" % (self.dev_name, cidr))
+            return 1
+
+        return 0
 
     def shutdown(self):
-        self.dev.down()
-        self.dev.close()
+
+        # bring tap device down
+        if run_cmd('ip link delete %s' % (self.dev_name), wait = True)[0]:
+            print("tap::up() : [ERROR] error bringing down %s tap device" % (self.dev_name))
+
+        self.fd.close()
+        return 0
 
     def print_info(self):
 
-        print("Tap::print_tap_dev_info() : [INFO] tap device info:")
-        print("\tname: %s" % (self.dev.name))
-        print("\taddr: %s" % (self.dev.addr))
-        print("\tnetmask: %s" % (self.dev.netmask))
-        print("\thwaddr: %s (%s)" % (self.dev.hwaddr, type(self.dev.hwaddr)))
-        print("\tmtu: %s" % (self.dev.mtu))
+        print("tap::print_info() : [INFO] tap device info:")
+        print("\tname: %s" % (self.dev_name))
+        print("\taddr: %s" % (self.net_addr))
+        print("\tnetmask: %s" % (self.net_mask))
+        print("\thwaddr: %s (%s)" % (self.hw_addr, type(self.hw_addr)))
+        print("\tmtu: %s" % (self.mtu))
